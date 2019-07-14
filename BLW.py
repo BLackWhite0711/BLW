635ba215b376291f92e3ff42246fc2214f4abd8e
BLackWhite <43264850+BLackWhite0711@users.noreply.github.com>
2019-07-15 00:42:14 +0700

    Update BLW.py

--- BLW.py
+++ BLW.py
@@ -1,4 +1,4 @@
-#Decompiled by MR.K7C8NG
+#Decompiled by MR.BLackWhite
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
